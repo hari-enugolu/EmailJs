b3b798455ef201e39e2e09d4531a866ac760e954
hari-enugolu <harienugolu123@gmail.com>
2022-03-22 23:59:32 +0530

    mailjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import emailjs from "emailjs-com";
 
-function App() {
+export default function App() {
+  function App(e) {
+    e.preventDefault();
+    emailjs
+      .sendform(
+        "service_8n9ucoo",
+        "template_1xrg8ge",
+        e.target,
+        "user_YPTEpTAF2CkKRxEjG"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form">
+        <div>
+          <div className="form">Email</div>
+          <input type="email" name="email"></input>
+        </div>
+        <div className="form">Username</div>
+        <input type="text" name="user_name"></input>
+
+        <div className="form">
+          Phone
+          <span>(optional)</span>
+        </div>
+        <input type="number" name="number"></input>
+        <div className="form">Amount</div>
+        <div>
+          <button onClick={App}>send</button>
+        </div>
+      </form>
     </div>
   );
 }
-
-export default App;
